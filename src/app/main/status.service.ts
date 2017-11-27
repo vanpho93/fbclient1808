5c631690e7a4820077c07f645b66eb73e160bcba
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-27 20:54:29 +0700

    client

--- src/app/main/status.service.ts
+++ src/app/main/status.service.ts
@@ -15,4 +15,13 @@ export class StatusService {
         .toPromise()
         .then(res => res.json());
     }
+
+    hitLike(idStatus: string) {
+        const url = 'http://localhost:3000/statuslike/' + idStatus;
+        const token = localStorage.getItem('token');
+        const headers = new Headers({ token });
+        return this.http.get(url, { headers })
+        .toPromise()
+        .then(res => res.json());
+    }
 }
--- src/app/main/status/status.component.ts
+++ src/app/main/status/status.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Status } from '../status';
 import { CommentService } from '../../comment.service';
+import { StatusService } from '../status.service';
 
 @Component({
   selector: 'app-status',
@@ -12,14 +13,22 @@ export class StatusComponent implements OnInit {
   @Input() status: Status;
   txtComment = '';
 
-  constructor(private commentService: CommentService) { }
+  constructor(
+    private commentService: CommentService,
+    private statusService: StatusService
+  ) { }
 
   ngOnInit() {
   }
 
   onSubmit(formValue) {
     this.commentService.sendComment(this.status._id, formValue.comment)
-    .then(x => console.log(x));
+    .then(comment => this.status.comments.push(comment));
     this.txtComment = '';
+  }
+
+  onHitLike() {
+    this.statusService.hitLike(this.status._id)
+    .then(x => console.log(x));
   }
 }
